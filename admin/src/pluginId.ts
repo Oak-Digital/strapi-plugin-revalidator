29870033d9008d08f0ef7b4e9e742b77d6ffc9c1
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-04-19 10:42:54 +0200

    Fixed plugin id

--- admin/src/pluginId.ts
+++ admin/src/pluginId.ts
@@ -1,5 +1,6 @@
 import pluginPkg from '../../package.json';
 
-const pluginId = pluginPkg.name.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
+/* const pluginId = pluginPkg.name.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, ''); */
+const pluginId = "revalidator";
 
 export default pluginId;
